e2d074e5dade55fce5d7d266301ae2142b110c5b
Pranshu Saini <ps1442003@outlook.com>
2025-06-26 22:56:20 +0530

    Deletion of Linked List

--- LinkedList/IntroLinkedList.cpp
+++ LinkedList/IntroLinkedList.cpp
@@ -67,9 +67,17 @@ void printLL(Node *head)
     cout << endl;
 }
 
-// Deletion and Insertion
+// Deletion
 Node *deleteAtHead(Node *head);
 Node *deleteAtTail(Node *head);
+Node *deleteAtKth(Node *head, int k);
+Node *deleteAtValue(Node *head, int value);
+
+// Insertion
+Node *insertAtHead(Node *head, int data);
+Node *insertAtTail(Node *head, int data);
+Node *insertAtKth(Node *head, int data, int k);
+Node *InsertAtValue(Node *head, int data, int value);
 
 int main()
 {
@@ -96,5 +104,100 @@ Node *deleteAtHead(Node *head)
     Node *temp = head;
     head = head->next;
     delete temp;
+    return head;
+}
+
+Node *deleteAtTail(Node *head)
+{
+    if (head == NULL)
+        return NULL;
+    if (head->next == NULL)
+    {
+        delete head;
+        return NULL;
+    }
+    Node *temp = head;
+    while (temp->next->next)
+    {
+        temp = temp->next;
+    }
+    delete temp->next;
+    temp->next = nullptr;
+    return head;
+}
+
+Node *deleteAtKth(Node *head, int k)
+{
+    if (head == NULL)
+        return head;
+    if (k == 1)
+    {
+        Node *temp = head;
+        head = head->next;
+        delete temp;
+        return head;
+    }
+    Node *curr = head;
+    for (int i = 1; curr != NULL && i < k - 1; i++)
+    {
+        curr = curr->next;
+    }
+    // Checks for out of bound.
+    if (curr == NULL || curr->next == NULL)
+        return head;
+    Node *nodeToDelete = curr->next;
+    curr->next = curr->next->next;
+    delete nodeToDelete;
+    return head;
+}
+
+Node *deleteAtValue(Node *head, int value)
+{
+    if (head == nullptr)
+        return nullptr;
+
+    if (head->data == value)
+    {
+        Node *temp = head;
+        head = head->next;
+        delete temp;
+        return head;
+    }
+    Node *prev = head;
+    Node *temp = head->next;
+    while (temp != nullptr)
+    {
+        if (temp->data == value)
+        {
+            prev->next = temp->next;
+            delete temp;
+            return head;
+        }
+        prev = temp;
+        temp = temp->next;
+    }
+    return head;
+}
+
+Node *insertAtHead(Node *head, int data)
+{
+    Node *newHead = new Node(data);
+    newHead->next = head;
+    return newHead;
+}
+
+Node *insertAtTail(Node *head, int data)
+{
+    if (head == nullptr)
+    {
+        return new Node(data);
+    }
+    Node *newTail = new Node(data);
+    Node *temp = head;
+    while (temp->next)
+    {
+        temp = temp->next;
+    }
+    temp->next = newTail;
     return head;
 }
