622b1bf1fff03f7421a23964220f0beb4c3beada
ProtoSol <ps1442003@outlook.com>
2025-06-30 12:57:29 +0530

    Reverse of LL

--- LinkedList/ReverseOfLL.cpp
+++ LinkedList/ReverseOfLL.cpp
@@ -1,14 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Define the Node structure
 struct Node {
   int data;
   Node* next;
   Node(int val) : data(val), next(nullptr) {}
 };
 
-// Function to print the linked list
 auto printList = [](Node* head) {
   Node* curr = head;
   while (curr) {
@@ -17,6 +15,11 @@ auto printList = [](Node* head) {
   }
   cout << endl;
 };
+
+// Reversing a LinkedList using Iteration
+Node* reverseLLIter(Node* head);
+// Reversing a LinkedList using Recursion
+Node* reverseLLRec(Node* head);
 
 int main() {
   // Create a simple linked list: 1 -> 2 -> 3 -> 4
@@ -29,4 +32,35 @@ int main() {
   printList(head);
 
   return 0;
+}
+
+Node* reverseLLIter(Node* head) {
+  if (head == NULL) return head;
+  Node* temp = head;
+  Node* prev = nullptr;
+  Node* front;
+  while (temp) {
+    front = temp->next;
+    temp->next = prev;
+    prev = temp;
+    temp = front;
+  }
+  return prev;
+}
+
+Node* reverseLLRec(Node* head) {
+  // Base case
+  if (head == NULL || head->next == NULL) return head;
+  // Get the new head
+  Node* newHead = reverseLLRec(head->next);
+
+  // It will return like this, So we just have to return
+  // 3 -> 4 <- 5
+  //      |
+  //      X
+  head->next->next = head;
+  head->next = nullptr;
+
+  // Return the new Head
+  return newHead;
 }
